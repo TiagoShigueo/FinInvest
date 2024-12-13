b7e98776299c451a685ebe613f2428d82db01c74
TiagoShigueo <tiago.yamasaki@gmail.com>
2024-12-12 23:30:59 -0300

    [ADD] Projeto base back-end Spring Boot

---
+++ back/src/main/java/com/fininvest/FinInvestApplication.java
@@ -0,0 +1,13 @@
+package com.fininvest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinInvestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinInvestApplication.class, args);
+	}
+
+}
