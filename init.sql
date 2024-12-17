cb149e0fd5a5ed15a197068839cf1be4da134e75
TiagoShigueo <tiago.yamasaki@gmail.com>
2024-12-17 04:30:43 -0300

    [ADD] Inicialização da tabela Users

--- init.sql
+++ init.sql
@@ -1,8 +1,9 @@
 CREATE TABLE IF NOT EXISTS users (
-    id_user INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255),
-    email VARCHAR(255),
-    password VARCHAR(255)
+    id_user INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR(20) NOT NULL
 );
 
-INSERT INTO users (name, email, password) VALUES ("Tiago", "tiago@email.com", "tiago");
+INSERT INTO users (name, email, password, role) VALUES ("Tiago", "tiago@email.com", "$2a$10$ccOdQijprWoRyqQZyTwN/uSFBzPPcqfnkTstnvhShuLPfLysPfVLW", 1); -- Senha Shigueo
