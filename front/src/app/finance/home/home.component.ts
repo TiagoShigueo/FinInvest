8a96a7838ea103c0a582601f522384d38cac54b1
TiagoShigueo <tiago.yamasaki@gmail.com>
2024-12-20 19:43:07 -0300

    [ADD] Projeto base da home financeiro

--- front/src/app/finance/home/home.component.ts
+++ front/src/app/finance/home/home.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { LoginService } from '../../auth/services/login.service';
 
 @Component({
   selector: 'app-home',
   standalone: false,
-  
+
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  constructor(private loginService: LoginService) {
+    console.log(localStorage);
+  }
+  onLogout() {
+    this.loginService.logout();
+  }
 }
