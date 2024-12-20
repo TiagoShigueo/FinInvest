0d9d8da7ffff96ff0d78dc0ecb4edabfcbffccb9
TiagoShigueo <tiago.yamasaki@gmail.com>
2024-12-20 19:40:23 -0300

    [ADD] Restrição das páginas por login

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
+import { HomeComponent } from './finance/home/home.component';
+import { AuthGuard } from './auth/auth.guard';
+
 const routes: Routes = [
   {
     path: '',
     component: LoginComponent,
+  },
+  {
+    path: 'finance/home',
+    component: HomeComponent,
+    canActivate: [AuthGuard],
   },
 ];
 
