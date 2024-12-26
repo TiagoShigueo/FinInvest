833a18522fdf9da3398fc294766a16948bff60a1
TiagoShigueo <tiago.yamasaki@gmail.com>
2024-12-25 22:55:19 -0300

    [FIX] Export do component da navbar

--- front/src/app/finance/finance.module.ts
+++ front/src/app/finance/finance.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
+import { TransactionComponent } from './transaction/transaction.component';
 
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    TransactionComponent
   ],
   imports: [
     CommonModule
