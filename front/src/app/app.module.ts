f522626fc4e425a56812a3f1431f98fb8252ff19
TiagoShigueo <tiago.yamasaki@gmail.com>
2024-12-26 00:12:33 -0300

    [ADD] Provide com as configurações do Brasil

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -9,6 +9,10 @@ import { AuthModule } from './auth/auth.module';
 import { FinanceModule } from './finance/finance.module';
 import { HttpClientModule } from '@angular/common/http';
 import { LayoutModule } from './layout/layout.module';
+import { registerLocaleData } from '@angular/common';
+import localePtBr from '@angular/common/locales/pt';
+
+registerLocaleData(localePtBr, 'pt-BR');
 
 @NgModule({
   declarations: [AppComponent],
@@ -22,7 +26,10 @@ import { LayoutModule } from './layout/layout.module';
     HttpClientModule,
     LayoutModule,
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'pt-BR' },
+    { provide: DEFAULT_CURRENCY_CODE, useValue: 'BRL' },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
