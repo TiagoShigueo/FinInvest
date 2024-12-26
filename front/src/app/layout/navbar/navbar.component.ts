f6aefa98a78d3a516a02db26ae94b2d946cda2a2
TiagoShigueo <tiago.yamasaki@gmail.com>
2024-12-25 22:54:14 -0300

    [ADD] Configuração base da navbar

--- front/src/app/layout/navbar/navbar.component.ts
+++ front/src/app/layout/navbar/navbar.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { LoginService } from '../../auth/services/login.service';
 
 @Component({
   selector: 'app-navbar',
   standalone: false,
-  
+
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-
+  constructor(private loginService: LoginService) {}
+  onLogout() {
+    this.loginService.logout();
+  }
 }
